cacc2d8f6e13075780dac8a617f0f8ee0c114453
CMonkey <42.codemonkey@gmail.com>
2020-10-21 13:14:58 +0800

    add getById getCategories functions

--- src/main/scala/org/excavator/boot/reactive/service/CategoryService.scala
+++ src/main/scala/org/excavator/boot/reactive/service/CategoryService.scala
@@ -1,5 +1,39 @@
 package org.excavator.boot.reactive.service
 
-class CategoryService {
+import java.util.UUID
+import java.util.stream.Collectors
+
+import org.excavator.boot.reactive.mapper.CategoryMapper
+import org.excavator.boot.reactive.repository.ICategoryRepository
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+import org.springframework.util.Assert
+
+@Service
+class CategoryService(repository:ICategoryRepository, mapper:CategoryMapper) {
+
+  val log = LoggerFactory.getLogger(classOf[CategoryService])
+
+  def getById(id: UUID) = {
+    Assert.notNull(id, "The given id must not be null")
+
+    log.debug("Try to get Category with id {}", id)
+
+    val category = repository.getOne(id)
+
+    if(null == category){
+      log.error("Category with id {} does not exist.", id)
+      null
+    }
+
+    log.debug("Category {} with id {} was found.", category.getTitle(), id)
+
+    category
+  }
+
+  def getCategories() = {
+    repository.findAll.stream.map( c => mapper.entityToDto(c))
+    .collect(Collectors.toList)
+  }
 
 }
