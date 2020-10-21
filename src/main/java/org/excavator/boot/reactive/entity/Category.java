f282a05989d47d86a53f4c5c1fbe4ca837ca78e4
CMonkey <42.codemonkey@gmail.com>
2020-10-21 13:06:19 +0800

    add fields to Category

--- src/main/java/org/excavator/boot/reactive/entity/Category.java
+++ src/main/java/org/excavator/boot/reactive/entity/Category.java
@@ -1,2 +1,46 @@
-package org.excavator.boot.reactive.entity;public class Category {
+package org.excavator.boot.reactive.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.OffsetDateTime;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Category {
+    public Category(String title) {
+        this.id = UUID.randomUUID();
+        this.title = title;
+    }
+
+    @Id
+    @Column(nullable = false, updatable = false)
+    private UUID id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String title;
+
+    @OneToMany(mappedBy = "category", targetEntity = Course.class, fetch = FetchType.LAZY)
+    private Set<Course> courses;
+
+    @Column(nullable = false, updatable = false)
+    private OffsetDateTime dateCreated;
+
+    @Column(nullable = false)
+    private OffsetDateTime lastUpdated;
+
+    @PrePersist
+    public void prePersist(){
+        this.dateCreated = OffsetDateTime.now();
+        this.lastUpdated = this.dateCreated;
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        this.lastUpdated = OffsetDateTime.now();
+    }
 }
