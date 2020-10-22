fe23862894284b651dba15b0b431a4687eaa45d8
CMonkey <42.codemonkey@gmail.com>
2020-10-22 09:44:40 +0800

    add entityToDto commandToEntity

--- src/main/scala/org/excavator/boot/reactive/mapper/CourseMapper.scala
+++ src/main/scala/org/excavator/boot/reactive/mapper/CourseMapper.scala
@@ -1,5 +1,10 @@
-package org.excavator.boot.reactive.service
+package org.excavator.boot.reactive.mapper
+
+import org.excavator.boot.reactive.entity.{Course, CreateCourse}
 
 class CourseMapper {
+  def entityToDto(c: Course)  = c
+
+  def commandToEntity(command: CreateCourse) = command
 
 }
