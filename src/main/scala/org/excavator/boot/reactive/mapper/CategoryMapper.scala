0c6341505fe34d947dd64f7370fa2622e1f8abe4
CMonkey <42.codemonkey@gmail.com>
2020-10-22 09:44:21 +0800

    change entityToDto

--- src/main/scala/org/excavator/boot/reactive/mapper/CategoryMapper.scala
+++ src/main/scala/org/excavator/boot/reactive/mapper/CategoryMapper.scala
@@ -3,6 +3,6 @@ package org.excavator.boot.reactive.mapper
 import org.excavator.boot.reactive.entity.Category
 
 class CategoryMapper {
-  def entityToDto(c: Category): R_ = ???
+  def entityToDto(c: Category) = c
 
 }
